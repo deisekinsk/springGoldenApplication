4d175e84e1178380744be63b19673db5b8d77fc1
deisekinsk <kinsk@msn.com>
2025-03-24 19:46:07 -0300

    Start Spring Project Application

---
+++ service-golden-service/src/main/java/com/dev_bootcamp/service_golden_service/ServiceGoldenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev_bootcamp.service_golden_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGoldenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGoldenServiceApplication.class, args);
+	}
+
+}
---
+++ service-golden-service/src/test/java/com/dev_bootcamp/service_golden_service/ServiceGoldenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_bootcamp.service_golden_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGoldenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
